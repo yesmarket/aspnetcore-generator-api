24e3bec210450311f332272578ec46f7e8561c62
Ryan Bartsch <rbartsch@adam.com.au>
2018-03-06 23:02:30 +1030

    added sort test

--- tests/RangeTests.cs
+++ tests/RangeTests.cs
@@ -17,15 +17,15 @@ namespace tests
             Assert.Equal(3, generated.Count());
         }
         
-        // [Fact]
-        // public void SortShouldOrderResults()
-        // {
-        //     var range = new Range { Count = 3, Sort = true };
-        //     var values = new[] { "a", "c", "b" };
-        //     var counter = 0;
-        //     var generated = range.Of(() => values[counter++]);
+        [Fact]
+        public void SortShouldOrderResults()
+        {
+            var range = new Range { Count = 3, Sort = true };
+            var values = new[] { "a", "c", "b" };
+            var counter = 0;
+            var generated = range.Of(() => values[counter++]);
 
-        //     Assert.Equal(new[] { "a", "b", "c" }, generated.ToArray());
-        // }
+            Assert.Equal(new[] { "a", "b", "c" }, generated.ToArray());
+        }
     }
 }
